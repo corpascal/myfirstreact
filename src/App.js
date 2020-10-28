5f23768ce226c46d8a4f813e06b04342b29f2833
corpascal <corpascal@gmail.com>
2020-10-28 08:26:21 +0100

    20201028 - 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     super();
 
     console.log('App.constructor - entrée2');
-    this.state = {restos: []};
+    this.state = {restos: [], currentId:'', currentName:'', currentImage:'', currentDescription:''};
 
     this.hostname = 'localhost';
     if (process.env.NODE_ENV == 'production')
@@ -32,9 +32,57 @@ class App extends Component {
     });
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const nouveauResto = {
+      id: this.state.currentId,
+      name: this.state.currentName,
+      image: this.state.currentImage,
+      description: this.state.currentDescription
+    };
+
+    const restos = this.state.restos.slice();
+    restos.push(nouveauResto);
+    this.setState({restos});
+  }
+
+  handleChangeId = (event) => {
+    const value = event.currentTarget.value;
+    this.setState({currentId:value});
+
+  }
+
+  handleChangeName = (event) => {
+    const value = event.currentTarget.value;
+    this.setState({currentName:value});
+
+  }
+
+  handleChangeDescription = (event) => {
+    const value = event.currentTarget.value;
+    this.setState({currentDescription:value});
+
+  }
+
+  handleSelect = (resto) => {
+    console.log('handleselect');
+    this.setState({currentId: resto.id});
+    this.setState({currentName: resto.name});
+    this.setState({currentImage: resto.image});
+    this.setState({currentDescription: resto.description});
+  }
 
   handleDelete = (id) => {
-    alert(id);
+    // Effectue une copie du tableau
+    const restos = this.state.restos.slice();
+
+    // Renvoie l'index dans le tableau de l'élément correspond à l'id transmis en paramètre
+    const index = restos.findIndex(resto => resto.id === id);
+
+    // Supprime l'élément du tableau
+    restos.splice(index,1);
+
+    this.setState({restos: restos});
   }
 
 
@@ -44,13 +92,28 @@ class App extends Component {
     return (
       <div>
         <h1>Liste des restos</h1>
-        <ul>
-            {this.state.restos.map((resto) => (
-              <li>
-                {resto.name} <button onClick={() => this.handleDelete(resto.id)}>X</button>
-              </li>
-            ))}
-        </ul>
+        <table>
+          <tr>
+            <th>id</th><th>name</th><th>image</th><th>description</th>
+          </tr>
+          {this.state.restos.map((resto) => (
+            <tr onClick={() => this.handleSelect(resto)}>
+                <td>{resto.id}</td> 
+                <td>{resto.name}</td> 
+                <td>{resto.image}</td> 
+                <td>{resto.description}</td> 
+                <td><button onClick={() => this.handleDelete(resto.id)}>X</button></td>
+            </tr>
+          ))}
+        </table>
+
+        <form onSubmit={this.handleSubmit}>
+            <input type='text' placeholder='id' value={this.state.currentId} onChange={this.handleChangeId}/><br/>
+            <input type='text' placeholder='name' value={this.state.currentName} onChange={this.handleChangeName}/><br/>
+            <input type='text' placeholder='image'/><br/>
+            <input type='text' placeholder='description' value={this.state.currentDescription} onChange={this.handleChangeDescription}/><br/>
+            <button>Ajouter</button>
+        </form>
       </div>
     );
   }
